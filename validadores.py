9bc96583fc75629f53b92a8dbe0bdc53256ceb37
Leonardo Varela Vacari <leonardo.vv1@puccampinas.edu.br>
2026-05-23 16:32:08 -0300

    feat: Validando o ID do pedido

--- validadores.py
+++ validadores.py
@@ -1,5 +1,5 @@
-def gerenciar_entrada_numerica(min_val, max_val):
-    escolha = input("Digite uma opção: ")
+def gerenciar_entrada_numerica(min_val, max_val, mensagem):
+    escolha = input(mensagem)
 
     if not escolha.isdigit():
         print(f"\nPor favor, insira uma opção entre {min_val} e {max_val}")
@@ -16,5 +16,17 @@ def validar_id_entregador(id_entregador):
         return False
     elif len(id_entregador) != 4:
         print("\nID do entregador deve conter apenas 4 dígitos")
+        return False
+    return True
+
+def validar_id_pedido(id_pedido):
+    if len(id_pedido) != 5:
+        print("ID deve conter 5 dígitos")
+        return False
+    elif not id_pedido[0].isalpha():
+        print("Primeiro dígito do ID deve ser uma letra")
+        return False
+    elif not id_pedido[1:].isdigit():
+        print("Quatro últimos dígitos do ID devem ser um número")
         return False
     return True
