d56246a260fa51789d04fd10946ad4f296cdddeb
Leonardo Varela Vacari <leonardo.vv1@puccampinas.edu.br>
2026-05-23 16:31:49 -0300

    feat: Criação da atualização de pedidos (Match case bugado e não está desassociando entregador, vou corrigir ainda)

--- pedidos.py
+++ pedidos.py
@@ -1,4 +1,4 @@
-from validadores import gerenciar_entrada_numerica, validar_id_entregador
+from validadores import gerenciar_entrada_numerica, validar_id_entregador, validar_id_pedido
 
 # lista_pedidos = [] ESTÁ LISTA DEVE ESTAR FORA DA FUNÇÃO NO ARQUIVO PRINCIPAL PARA ADICIONAR OS PEDIDOS
 lista_pedidos = []
@@ -29,18 +29,22 @@ def cadastrar_pedido():
     pedido["id_pedido"] = gerar_id_pedido()
     pedido["nome_cliente"] = input("Insira o nome do cliente: ")
 
-    print("\nPRIORIDADE \n[1] ALTA \n[2] NORMAL")
-    escolha_prioridade = gerenciar_entrada_numerica(1,2)
+    escolha_prioridade = gerenciar_entrada_numerica(1,2, "\nPRIORIDADE \n[1] ALTA \n[2] NORMAL "
+                                                         "\nDigite uma opção:")
     while not escolha_prioridade:
-        escolha_prioridade = gerenciar_entrada_numerica(1,2)
+        escolha_prioridade = gerenciar_entrada_numerica(1,2, "\nPRIORIDADE \n[1] ALTA \n[2] NORMAL "
+                                                             "\nDigite uma opção:")
     pedido["prioridade"] = escolha_prioridade
 
     pedido["descricao_pedido"] = input("Insira a descrição do produto: ")
 
-    print("\nSTATUS DO PEDIDO \n[1] PENDENTE \n[2] EM ROTA \n[3] ENTREGUE \n[4] CANCELADO")
-    escolha_status = gerenciar_entrada_numerica(1, 4)
+    escolha_status = gerenciar_entrada_numerica(1, 4, "\nSTATUS DO PEDIDO \n[1] PENDENTE "
+                                                      "\n[2] EM ROTA \n[3] ENTREGUE \n[4] CANCELADO "
+                                                      "\nDigite uma opção:")
     while not escolha_status:
-        escolha_status = gerenciar_entrada_numerica(1,4)
+        escolha_status = gerenciar_entrada_numerica(1,4, "\nSTATUS DO PEDIDO \n[1] PENDENTE "
+                                                         "\n[2] EM ROTA \n[3] ENTREGUE \n[4] CANCELADO "
+                                                         "\nDigite uma opção:")
     pedido["status_pedido"] = escolha_status
 
     id_entregador = input("Insira o ID do entregador responsável: ")
@@ -62,3 +66,61 @@ def cadastrar_pedido():
     print(f"STATUS -> {status_texto}")
     print(f"ID ENTREGADOR -> {pedido["id_entregador"]}")
 
+
+def buscar_posicao_por_id(id_procurado):
+    for i, pedido in enumerate(lista_pedidos):
+        if pedido['id_pedido'] == id_procurado:
+            return i
+
+    return -1
+
+
+def atualizar_pedido():
+    if not lista_pedidos:
+        print("Sem pedidos para atualizar...")
+        return False
+
+    id_pedido = input("Digite o ID do pedido que deseja atualizar: ")
+    while not validar_id_pedido(id_pedido):
+        id_pedido = input("Digite o ID do pedido que deseja atualizar: ")
+
+    posicao = buscar_posicao_por_id(id_pedido)
+
+    if posicao == -1:
+        print("Pedido não encontrado na base de dados.")
+        return False
+
+    escolha = gerenciar_entrada_numerica(1, 3,
+                                         "[1] Alterar Status do Pedido \n[2] Editar Entregador \n[3] Desassociar Entregador\nEscolha uma opção: ")
+
+    match escolha:
+        case 1:
+            escolha_status = gerenciar_entrada_numerica(1, 4,
+                                                        "\nSTATUS DO PEDIDO \n[1] PENDENTE \n[2] EM ROTA \n[3] ENTREGUE \n[4] CANCELADO \nDigite uma opção: ")
+            while not escolha_status:
+                escolha_status = gerenciar_entrada_numerica(1, 4,
+                                                            "\nSTATUS DO PEDIDO \n[1] PENDENTE \n[2] EM ROTA \n[3] ENTREGUE \n[4] CANCELADO \nDigite uma opção: ")
+
+            status_opcoes = ["", "PENDENTE", "EM ROTA", "ENTREGUE", "CANCELADO"]
+            status_texto = status_opcoes[escolha_status]
+
+            lista_pedidos[posicao]["status_pedido"] = status_texto
+            print(f"Status do pedido atualizado para: {status_texto}")
+            return True
+
+        case 2:
+            id_entregador = input("Insira o ID do entregador responsável: ")
+            while not validar_id_entregador(id_entregador):
+                id_entregador = input("Insira o ID do entregador responsável: ")
+            lista_pedidos[posicao]["id_entregador"] = id_entregador
+            print("Entregador atualizado com sucesso!")
+            return True
+
+        case 3:
+            if lista_pedidos[posicao]["id_entregador"] == "0000":
+                print("Pedido já está sem entregador associado.")
+            else:
+                lista_pedidos[posicao]["id_entregador"] = "0000"
+                print("Entregador desassociado do pedido.")
+            return True
+    return None
