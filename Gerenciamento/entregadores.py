9b8962e076e349c0ed23f0c18f64eeecf6c63af0
Vinicius Hegues Baiocco <vinicius.baiocco@gmail.com>
2026-05-28 08:13:31 -0300

    feat: cadastro de entregadores

---
+++ Gerenciamento/entregadores.py
@@ -0,0 +1,78 @@
+from Validacao.validadores import gerenciar_entrada_numerica, validar_nome
+from utils import limpar_tela, confirmacao
+from colorama import Style, Fore
+from Menu.sub_menus import sub_menu_estados, sub_menu_veiculo, sub_menu_regiao
+import random
+
+def gerar_id_entregador():
+    return str(random.randint(1000, 9999))
+
+def cadastrar_nome_entregador():
+    limpar_tela()
+    nome = input(Fore.WHITE + Style.BRIGHT + "Insira o nome do entregador: ")
+    while not validar_nome(nome):
+        limpar_tela()
+        nome = input(Fore.YELLOW + Style.BRIGHT + "Insira o nome do entregador novamente: ")
+    limpar_tela()
+    return nome.upper()
+
+def cadastrar_veiculo():
+    limpar_tela()
+    sub_menu_veiculo()
+    veiculo = gerenciar_entrada_numerica(1, 3, "\nDigite uma opção: ")
+    while not veiculo:
+        limpar_tela()
+        sub_menu_veiculo()
+        veiculo = gerenciar_entrada_numerica(1, 3, "\nDigite uma opção novamente: ")
+    return veiculo
+
+def cadastrar_estado_entregador():
+    limpar_tela()
+    sub_menu_estados()
+    estado = gerenciar_entrada_numerica(1, 7, "Digite uma opção: ")
+    while not estado:
+        limpar_tela()
+        sub_menu_estados()
+        estado = gerenciar_entrada_numerica(1, 7, "Digite uma opção novamente: ")
+    estados_norte = ["", "AC", "AP", "AM", "PA", "RO", "RR", "TO"]
+    return estados_norte[estado]
+
+def cadastrar_regiao_entregador():
+    limpar_tela()
+    sub_menu_regiao()
+    regiao = gerenciar_entrada_numerica(1, 5, "\nDigite uma opção: ")
+    while not regiao:
+        limpar_tela()
+        sub_menu_regiao()
+        regiao = gerenciar_entrada_numerica(1, 5, "\nDigite uma opção novamente: ")
+    return str(regiao)
+
+def cadastrar_entregador(lista_entregadores):
+    campos = ["id_entregador", "nome_entregador", "veiculo", "estado", "regiao", "disponibilidade"]
+    entregador = dict.fromkeys(campos)
+
+    entregador["id_entregador"] = gerar_id_entregador()
+    entregador["nome_entregador"] = cadastrar_nome_entregador()
+    entregador["veiculo"] = cadastrar_veiculo()
+    entregador["estado"] = cadastrar_estado_entregador()
+    entregador["regiao"] = cadastrar_regiao_entregador()
+    entregador["disponibilidade"] = "DISPONÍVEL"
+
+    lista_entregadores.append(entregador)
+
+    if entregador["veiculo"] == 1:
+        veiculo_texto = "MOTO"
+    elif entregador["veiculo"] == 2:
+        veiculo_texto = "CARRO"
+    else:
+        veiculo_texto = "VAN"
+
+    limpar_tela()
+    print(Fore.WHITE + Style.BRIGHT + "-----ENTREGADOR CADASTRADO-----")
+    print(f"ID -> {entregador['id_entregador']}")
+    print(f"NOME -> {entregador['nome_entregador']}")
+    print(f"VEÍCULO -> {veiculo_texto}")
+    print(f"ESTADO -> {entregador['estado']}")
+    print(f"REGIÃO -> {entregador['regiao']}")
+    print(f"DISPONIBILIDADE -> {entregador['disponibilidade']}")
+    confirmacao()
