216283c454cf0ddea6def9d7e08ae966fb01bc2b
Vinicius Hegues Baiocco <vinicius.baiocco@gmail.com>
2026-06-01 08:06:22 -0300

    removi todos os arquivos, deixando só main no projeto

--- utils.py
+++
@@ -1,8 +0,0 @@
-import os
-from cores import BRANCO
-
-def limpar_tela():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-def confirmacao():
-    input(BRANCO + "\nPressione Enter para voltar ao menu...")
